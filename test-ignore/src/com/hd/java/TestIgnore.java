492b2dfe29f71f8316d599ddf5792f5b5b1c7b70
Moonxy <945561359@qq.com>
2019-06-28 16:00:48 +0800

    添加ignoretest到git

---
+++ test-ignore/src/com/hd/java/TestIgnore.java
@@ -0,0 +1,16 @@
+package com.hd.java;
+
+/**
+ * @ClassName TestIgnore
+ * @Description TODO
+ * @Author XyWorkPC
+ * @Date 2019/6/28 15:59
+ * @Version 1.0
+ **/
+
+public class TestIgnore {
+
+    public static void main(String[] args) {
+        System.out.println("qqqqqq");
+    }
+}
